de270aee230061d23c545dcc7649fb91a851a2ed
neitoo <samuraa855@gmail.com>
2024-03-02 15:26:00 +0500

    Реализовано переключение карточек и подгрузка блока  в зависимости от экрана

--- script/script.js
+++ script/script.js
@@ -61,13 +61,158 @@ function createMobileSupportBlock() {
     mainContent.appendChild(supportDiv);
 }
 
+function createStagesBlock(){
+    const cardStages = document.querySelector('.card-stages');
+    cardStages.innerHTML = `
+        <div class="card card-1">
+            <div class="card-number">1</div>
+            <div class="card-text">Строительство железнодорожной магистрали Москва-Васюки</div>
+        </div>
+        <div class="card card-2">
+            <div class="card-number">2</div>
+            <div class="card-text">
+                Открытие фешенебельной гостиницы «Проходная пешка» и других небоскрёбов
+            </div>
+        </div>
+        <div class="card card-3">
+            <div class="card-number">3</div>
+            <div class="card-text">
+                Поднятие сельского хозяйства в радиусе на тысячу километров: производство овощей, фруктов, икры, шоколадных конфет
+            </div>
+        </div>
+        <div class="card card-4">
+            <div class="card-number">4</div>
+            <div class="card-text">Строительство дворца для турнира</div>
+        </div>
+        <div class="card card-5">
+            <div class="card-number">5</div>
+            <div class="card-text">Размещение гаражей для гостевого автотранспорта</div>
+        </div>
+        <div class="card card-6">
+            <div class="card-number">6</div>
+            <div class="card-text">
+                Постройка сверхмощной радиостанции для передачи всему миру сенсационных результатов
+            </div>
+        </div>
+        <div class="card card-7">
+            <div class="card-number">7</div>
+            <div class="card-text">
+                Создание аэропорта «Большие Васюки» с регулярным отправлением почтовых самолётов и дирижаблей во все концы света, включая Лос-Анжелос и Мельбурн
+            </div>
+            <img src="./source/airplane.png">
+        </div>
+    `;
+}
+function createMobileStagesBlock(){
+    const cardStages = document.querySelector('.card-stages');
+    cardStages.innerHTML = `
+        <img class="airplane" src="./source/airplane.png" />
+        <div class="card card-1">
+            <div class="card-row">
+                <div class="card-number">1</div>
+                <div class="card-text">Строительство железнодорожной магистрали Москва-Васюки</div>
+            </div>
+            <div class="card-row">
+                <div class="card-number">2</div>
+                <div class="card-text">
+                    Открытие фешенебельной гостиницы «Проходная пешка» и других небоскрёбов
+                </div>
+            </div>
+        </div>
+        <div class="card card-2">
+            <div class="card-row">
+                <div class="card-number">3</div>
+                <div class="card-text">
+                    Поднятие сельского хозяйства в радиусе на тысячу километров: производство овощей, фруктов, икры, шоколадных конфет
+                </div>
+            </div>
+        </div>
+        <div class="card card-3">
+            <div class="card-row">
+                <div class="card-number">4</div>
+                <div class="card-text">Строительство дворца для турнира</div>
+            </div>
+            <div class="card-row">
+                <div class="card-number">5</div>
+                <div class="card-text">Размещение гаражей для гостевого автотранспорта</div>
+            </div>
+        </div>
+        <div class="card card-4">
+            <div class="card-row">
+                <div class="card-number">6</div>
+                <div class="card-text">
+                    Постройка сверхмощной радиостанции для передачи всему миру сенсационных результатов
+                </div>
+            </div>
+        </div>
+        <div class="card card-5">
+            <div class="card-row">
+                <div class="card-number">7</div>
+                <div class="card-text">
+                    Создание аэропорта «Большие Васюки» с регулярным отправлением почтовых самолётов и дирижаблей во все концы света, включая Лос-Анжелос и Мельбурн
+                </div>
+            </div>
+        </div>
+    `;
+}
+
 // Проверка ширины экрана и создание соответствующего блока поддержки
 function checkScreenWidth() {
     const screenWidth = window.innerWidth;
     if (screenWidth >= 1024) {
         createSupportBlock();
+        createStagesBlock();
     } else {
         createMobileSupportBlock();
+        createMobileStagesBlock();
+    }
+}
+
+const cardContainer = document.querySelector('.card-stages');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const dots = document.querySelectorAll('.dot');
+let currentIndex = 0;
+
+prevButton.addEventListener('click', () => {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    updateDots();
+    scrollCards(cardContainer.offsetWidth * -1);
+    updateButtons();
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = Math.min(currentIndex + 1, dots.length - 1);
+    updateDots();
+    scrollCards(cardContainer.offsetWidth);
+    updateButtons();
+});
+
+function scrollCards(scrollAmount) {
+    cardContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+}
+
+function updateDots() {
+    dots.forEach((dot, index) => {
+        if (index === currentIndex) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');
+        }
+    });
+}
+
+function updateButtons() {
+    if (currentIndex === 0) {
+        prevButton.classList.add('end');
+    } else {
+        prevButton.classList.remove('end');
+    }
+
+    if (currentIndex === dots.length - 1) {
+        nextButton.classList.add('end');
+    } else {
+        nextButton.classList.remove('end');
     }
 }
 
